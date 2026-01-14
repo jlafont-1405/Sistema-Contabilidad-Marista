88bcb5245aaf6c0993aab6b976c393fa7d58c6f9
Jean-Claude Lafont <jlafont.academy@gmail.com>
2026-01-14 12:55:26 -0400

    fix: Removiendo el import de nodemailer

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,7 +4,6 @@ import User from '../models/user.model';
 import Transaction from '../models/transaction.model'; 
 import Budget from '../models/budget.model';           
 import crypto from 'crypto';
-import nodemailer from 'nodemailer';
 import { Resend } from 'resend';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secreto_super_seguro_marista';
